32f291cbc562d9fb20a75e51115e6b194f757582
alxcolque <a.colq.c@hotmail.com>
2022-06-26 23:27:44 -0400

    controller send data to view

--- app/controllers/WelcomeController.py
+++ app/controllers/WelcomeController.py
@@ -5,5 +5,7 @@ class WelcomeController():
     def __init__(self):
         pass
     def index(self):
-        return render_template('welcome.html')
+        colores = {'color1':'negro','color2':'blanco','color3':'rojo'}
+        saludo = "hola cómo estás?"
+        return render_template('welcome.html', colores=colores, saludo = saludo)
 welcomecontroller = WelcomeController()
