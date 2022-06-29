c155605813a170bb087701aef3160a38f6ef2c14
alxcolque <a.colq.c@hotmail.com>
2022-06-28 22:42:26 -0400

    added template startBootstrap

--- app/controllers/WelcomeController.py
+++ app/controllers/WelcomeController.py
@@ -7,5 +7,5 @@ class WelcomeController():
     def index(self):
         colores = {'color1':'negro','color2':'blanco','color3':'rojo'}
         saludo = "hola cómo estás?"
-        return render_template('welcome.html', colores=colores, saludo = saludo)
+        return render_template('layout/app.html', colores=colores, saludo = saludo)
 welcomecontroller = WelcomeController()
