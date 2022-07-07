16f746d32b9353421de310ad0176fb54b15d1f36
alxcolque <a.colq.c@hotmail.com>
2022-07-06 22:11:17 -0400

    added crud of product

--- app/controllers/ProductController.py
+++ app/controllers/ProductController.py
@@ -1,10 +1,55 @@
-from flask import render_template
+from flask import flash, render_template, request, redirect, url_for, session
 from app.models.Product import Product
+from app import db, app
 class ProductController():
     def __init__(self):
         pass
     def index(self):
         products = Product.query.all()
         return render_template('products/index.html', products=products)
+    def create(self):
+        return render_template('products/create.html')
+    def store(self):
+        if request.method == 'POST':
+            description = request.form['description']
+            price = request.form['price']
+            type = request.form['type']
+            
+            new_product = Product(
+                description = description,
+                type = type,
+                price = price
+            )
+            db.session.add(new_product)
+            db.session.commit()
+
+            flash('Registro exitoso..!', 'success')
+            return redirect(url_for('product_router.index'))
+        return render_template('products/create.html')
+    def edit(self, _id):
+        product = db.session.query(Product).filter(Product.id==_id).one()
+        return render_template('products/edit.html', product=product)
+    def destroy(self, _id):
+        product = db.session.query(Product).filter(Product.id==_id).one()
+        db.session.delete(product)
+        db.session.commit()
+        flash('Producto eilminado con éxito..!', 'success')
+        return redirect(url_for('product_router.index'))
+    def update(self, _id):
+        if request.method == 'POST':
+            description = request.form['description']
+            price = request.form['price']
+            type = request.form['type']
+
+            product = db.session.query(Product).filter(Product.id==_id).one()
+            
+            product.description = description,
+            product.type = type,
+            product.price = price
+            db.session.commit()
+
+            flash('Registro actualizado exitosamente..!', 'success')
+            return redirect(url_for('product_router.index'))
+        return '404 Método no válido'
 
 productcontroller = ProductController()
--- app/routes/product_router.py
+++ app/routes/product_router.py
@@ -6,3 +6,20 @@ product_router = Blueprint('product_router', __name__)
 @product_router.route('/products', methods=['GET'])
 def index():
     return productcontroller.index()
+
+@product_router.route('/products/create', methods=['GET'])
+def create():
+    return productcontroller.create()
+
+@product_router.route('/products/store', methods=['POST'])
+def store():
+    return productcontroller.store()
+@product_router.route('/products/delete/<id>', methods=['GET'])
+def destroy(id):
+    return productcontroller.destroy(id)
+@product_router.route('/products/edit/<id>', methods=['GET'])
+def edit(id):
+    return productcontroller.edit(id)
+@product_router.route('/products/update/<id>', methods=['POST'])
+def update(id):
+    return productcontroller.update(id)
